050d1f55166b003345bb7d4c50254d724788a814
Meteor <caiyuanji@163.com>
2021-02-07 12:20:39 +0800

    init project

---
+++ src/main/java/cn/qianfeng/webdemo/helloController.java
@@ -0,0 +1,11 @@
+package cn.qianfeng.webdemo;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloController {
+    @RequestMapping("/")
+    public String hello() {
+        return "<h1> <font color=#ff0000> Hello QF devops -- v1.0.0 </font>";
+    }
+}
---
+++ src/main/resources/start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd `dirname $0`
+JARFILE=`ls *.jar`
+Package_name=${JARFILE%%.jar*}
+hostname=`hostname`
+CMD="-XX:+HeapDumpOnOutOfMemoryError \
+     -XX:+PrintGCDetails \
+     -XX:MaxGCPauseMillis=200 \
+     -Xloggc:/data/log/gclogs/${Package_name}_gc.log \
+     -XX:HeapDumpPath=/data/log/${Package_name}.hprof"
+pid=$(ps uxf|grep ${JARFILE}|grep -v grep|awk '{print $2}')
+if [[ -z $pid ]]; then
+    cd `dirname $0`
+    /usr/bin/java $CMD -jar ${JARFILE} 2>&1 >/dev/null
+    echo "${Package_name} started"
+else
+    echo "${Package_name} is already running"
+fi
